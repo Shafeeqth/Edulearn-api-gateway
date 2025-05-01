c42c9eb69040cdd124c4f51ed56f84e808b23a3c
shafeeque <shafeeqsha06@gmail.com>
2025-05-01 22:38:42 +0530

    feat(utility-serv): added a reusable utility validator service

---
+++ src/services/validate-schema.ts
@@ -0,0 +1,18 @@
+
+import { ValidationError } from "../utils/errors/validation-error";
+import {  ZodError,  ZodType } from "zod";
+
+export default function validateSchema<T extends ZodType>(
+  data: unknown,
+  schema: T
+): ReturnType<T["parse"]>  | undefined {
+  try {
+    return schema.parse(data);
+  } catch (error) {
+    if (error instanceof ZodError) {
+        
+        throw new ValidationError(error); 
+    }
+
+  }
+}
