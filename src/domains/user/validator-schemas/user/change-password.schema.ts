fbd4bdc0663a5c401e3bee24ea4029deadf27a37
shafeeque <shafeeqsha06@gmail.com>
2025-05-13 23:06:52 +0530

    feat(user-schema): add user domain specific validation schemas for change password request and current user

---
+++ src/domains/user/validator-schemas/user/change-password.schema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const changePasswordSchema = z.object({
+  userId: z.string().min(1, { message: "userId is required" }),
+
+  oldPassword: z.string().email({ message: "old password is required" }),
+  newPassword: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" })
+    .max(50)
+    .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).+$/, {
+      message:
+        "Password must include at least one digit, one lowercase letter, one uppercase letter, and one special character",
+    }),
+});
+
+// .refine((data) => data.password === data.confirmPassword, {
+//   message: "Passwords must match",
+//   path: ["confirmPassword"],
+// });
+
+export type ChangePasswordType = z.infer<typeof changePasswordSchema>;
