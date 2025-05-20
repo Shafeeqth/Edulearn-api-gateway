8a1962d66f8e6a13c40f796f6a170b42e9bb6b8e
shafeeque <shafeeqsha06@gmail.com>
2025-05-20 20:08:14 +0530

    feat(multer-middleware): add multer middleware to manage file and image uploads

---
+++ src/middlewares/upload.middleware.ts
@@ -0,0 +1,17 @@
+import multer from "multer";
+import { StorageMemory } from "../shared/constants/storage";
+import { fileFilter } from "../utils/fileFilter.utils";
+
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: StorageMemory.MB * 10, // 10 MB
+  },
+  fileFilter: fileFilter,
+});
+
+export const uploadSingleImage = upload.single("image");
+export const uploadSingleFile = upload.single("file");
+export const uploadMultipleFiles = upload.array("files", 5);
