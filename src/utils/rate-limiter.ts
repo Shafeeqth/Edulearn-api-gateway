58d8aa6db00820b07cdee61ddbb76097619f32f4
shafeeque <shafeeqsha06@gmail.com>
2025-05-20 20:15:13 +0530

    feat(utils): add utils for rate-limit, fileFilter

---
+++ src/utils/rate-limiter.ts
@@ -0,0 +1,36 @@
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import RedisService from "./redis";
+import redisClient from "./redis";
+
+export interface RateLimiterConfig {
+  keyPrefix?: string;
+  points?: number;
+  duration?: number;
+}
+
+export const defaultRateLimiterConfig: RateLimiterConfig = {
+  keyPrefix: "api_gateway-rate-limit",
+  points: 100,
+  duration: 60,
+};
+
+export class RateLimiter {
+  private limiter: RateLimiterRedis;
+
+  constructor(config: RateLimiterConfig = {}) {
+    const limiterConfig = { ...defaultRateLimiterConfig, ...config };
+    this.limiter = new RateLimiterRedis({
+      storeClient: redisClient.getClient(),
+      keyPrefix: limiterConfig.keyPrefix,
+      points: limiterConfig.points,
+      duration: limiterConfig.duration,
+    });
+  }
+
+  async consume(key: string): Promise<void> {
+    await this.limiter.consume(key).catch((e) => {
+      console.error("Error in Rate limiter :(", e);
+      throw e;
+    });
+  }
+}
