22a50c1ce6dce4cc8dc2e75d595656e277bf7be7
shafeeque <shafeeqsha06@gmail.com>
2025-05-21 10:39:23 +0530

    feat(routes): add few admin and few user routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,9 +1,11 @@
 import {Router} from "express";
 import { userRouterV1 } from "./user.router";
 import { authRouterV1 } from "./auth.router";
+import { adminRouterV1 } from "./admin.router";
 
 const router = Router();
 router.use(userRouterV1);
 router.use(authRouterV1);
+router.use(adminRouterV1);
 
 export {router as routerV1};
--- src/routes/v1/user.router.ts
+++ src/routes/v1/user.router.ts
@@ -1,7 +1,8 @@
+import { blocklistMiddleware } from "../../middlewares/blocklist.middleware";
 import { userRouter } from "../../domains/user/routers/v1/users/users.router";
 import {Router} from "express";
 
 const router = Router();
-router.use("/users", userRouter);
+router.use("/users",  userRouter);
 
 export {router as userRouterV1};
