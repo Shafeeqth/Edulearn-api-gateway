466fe9bf7f104d96d85f23ee89892d0cf328b380
shafeeque <shafeeqsha06@gmail.com>
2025-05-21 10:43:37 +0530

    feat(middleware): add auhenticattion and autherization middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -4,11 +4,22 @@ import jwt from "jsonwebtoken";
 import { AuthenticationError } from "../utils/errors/unauthenticate.error";
 
 type JwtPayload = {
-  username: string;
-  userId: string;
-  role: string;
-  avatar: string;
-};
+  user: {
+    username: string;
+    userId: string;
+    role: string;
+    avatar: string;
+  };
+} & StandardJwtClaims;
+
+export interface StandardJwtClaims {
+  iat: number;
+  iss: string;
+  aud: string;
+  jti: string;
+  exp?: number;
+  sub?: string;
+}
 
 export const authenticate = (
   req: Request,
@@ -26,11 +37,9 @@ export const authenticate = (
 
   try {
     const decoded = jwt.verify(token, config.accessTokenSecret) as JwtPayload;
-    req.user = decoded;
+    req.user = decoded.user;
   } catch (error) {
-    next(new AuthenticationError(
-      "Authentication failed: invalid token."
-    ));
+    next(new AuthenticationError("Authentication failed: invalid token."));
   }
   next();
 };
